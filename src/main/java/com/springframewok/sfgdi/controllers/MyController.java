fb6471925b3cedbce5cf538e5096d8af2d2e4304
Rafal <grafal96@gmail.com>
2020-02-26 18:20:50 +0100

    understanding Spring Context

---
+++ src/main/java/com/springframewok/sfgdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.springframewok.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello world");
+
+        return "Hi buds !";
+    }
+}
